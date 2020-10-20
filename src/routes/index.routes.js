02b701cbe0db8f269ad3307d758a155857235271
JavierBoscan <javierboscan16@gmail.com>
2020-10-19 20:37:06 -0400

    se agrego index.routes.js y se exporto su async function

---
+++ src/routes/index.routes.js
@@ -0,0 +1,31 @@
+
+
+let content = document.querySelector('#container-route')
+
+const router = async (route) => {
+    content.innerHTML = ''
+
+    switch (route) {
+        case '#/':{
+
+        }
+
+        case '#/habitat': {
+
+        }
+        
+        case '#/breding': {
+
+        }
+
+        case '#/food': {
+
+        }
+
+        default:{
+            
+        }
+    }
+}
+
+export { router }
